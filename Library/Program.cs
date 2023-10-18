8628652d748a35df542a751e51161d049a6aebe5
Артем Кунченко <kunchenko911@ukr.net>
2023-10-18 20:31:46 +0300

    added method IndexOf

--- Library/Program.cs
+++ Library/Program.cs
@@ -8,13 +8,20 @@ namespace Library
         static void Main(string[] args)
         {
             char[] str = new char[] { 'H', 'i', ',', ' ', 'A', 'l', 'e', 'x', '!' };
+            char[] search = new char[] { 'A', 'l', 'e', 'x' };
             MyString.String ex = new MyString.String(str);
-            Console.WriteLine(ex.StartWith());
-            Console.WriteLine(ex.EndWith());
-            Console.WriteLine(ex.ToString());
-            MyString.String ex2 = new MyString.String(ex.Substring(4,4));
-            Console.WriteLine(ex2.ToString());
+            //Console.WriteLine(ex.StartWith());
+            //Console.WriteLine(ex.EndWith());
+            //Console.WriteLine(ex.ToString());
+            //MyString.String ex2 = new MyString.String(ex.Substring(4,4));
+            //Console.WriteLine(ex2.ToString());
+            //ex.Remove(2, 6);
+            //Console.WriteLine(ex.ToString());
+            //ex2.RemoveAll();
+            //Console.WriteLine(ex2.ToString());
+            Console.WriteLine(ex.IndexOf(search));
             Console.ReadKey();
+            
         }
     }
 }
--- MyString/String.cs
+++ MyString/String.cs
@@ -3,34 +3,77 @@
     public class String
     {
         public char[] str { get; set; }
-        public int length { get; }
+        public int length { get; set; }
         public String(char[] str)
         {
             this.str = str;
             this.length = str.Length;
         }
-        public char StartWith() { return str[0]; }  
+        public char StartWith() { return str[0]; }
         public char EndWith() { return str[length - 1]; }
         public char[] Substring(int startIndex, int endIndex)
         {
             // if data in startIndex and endIndex aren't correct
             if (startIndex < 0) startIndex = 0;
-            if((startIndex+endIndex)>length) endIndex = length-startIndex;
+            if ((startIndex + endIndex) > length) endIndex = length - startIndex;
             if (endIndex < 0) endIndex = 0;
             //create new string
             int sub_lenght = endIndex;
-            char[] sub_string= new char[sub_lenght];
+            char[] sub_string = new char[sub_lenght];
             for (int i = 0; i < sub_lenght; i++)
             {
                 sub_string[i] = str[startIndex + i];
             }
             return sub_string;
         }
+        public void Remove(int startIndex, int endIndex)
+        {
+            // if data in startIndex and endIndex aren't correct
+            if (startIndex < 0) startIndex = 0;
+            if ((startIndex + endIndex) > length) endIndex = length - startIndex;
+            if (endIndex < 0) endIndex = 0;
+            int temp_lenght = this.length - endIndex;
+            char[] temp_string = new char[temp_lenght];
+            for (int i = 0, j = 0; i < this.length; i++)
+            {
+                if (i <startIndex||i>=(startIndex+endIndex))
+                {
+                    temp_string[j] = this.str[i];
+                    j++;
+                }
+
+            }
+            this.length = temp_lenght;
+            this.str = temp_string;
+        }
+        public void RemoveAll() 
+        {
+            this.length = 0;
+            this.str = new char[this.length];
+        }
+        public int IndexOf(char[] search)
+        {
+            int counter = 0;
+            for (int i = 0; i <length; i++)
+            {
+                if (this.str[i] == search[0])
+                {
+                    for (int j = 0; j < search.Length; j++)
+                    {
+                        if(this.str[i+j] == search[j]) counter++;
+                    }
+                }
+                if(counter==search.Length) return i;
+                counter = 0;
+              
+            }
+            return -1;
+        }
 
 
         public override string ToString()
         {
-            string str_print=new string(this.str);
+            string str_print = new string(this.str);
             return str_print;
         }
     }
