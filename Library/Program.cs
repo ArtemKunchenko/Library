66967efa175f8225ed91af93e7dceb71544ef551
Артем Кунченко <kunchenko911@ukr.net>
2023-10-18 17:59:55 +0300

    Add project files.

---
+++ Library/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyString;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = new char[] { 'H', 'i', ',', ' ', 'A', 'l', 'e', 'x', '!' };
+            MyString.String ex = new MyString.String(str);
+            Console.WriteLine(ex.StartWith());
+            Console.WriteLine(ex.EndWith());
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyString/String.cs
@@ -0,0 +1,16 @@
+﻿namespace MyString
+{
+    public class String
+    {
+        public char[] str { get; set; }
+        public int length { get; }
+        public String(char[] str)
+        {
+            this.str = str;
+            this.length = str.Length;
+        }
+        public char StartWith() { return str[0]; }  
+        public char EndWith() { return str[length - 1]; }
+
+    }
+}
